c5bfc097dc66e6a6a85ebc4ba77678558f699a67
KhateebKhan <121089418+KhateebKhan@users.noreply.github.com>
2026-04-08 01:16:20 +0200

    new

--- RestaurantAPI/Controllers/OrderController.cs
+++ RestaurantAPI/Controllers/OrderController.cs
@@ -1,15 +1,45 @@
 using Microsoft.AspNetCore.Mvc;
+using RestaurantAPI.Models;
 
 namespace RestaurantAPI.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class OrderController : ControllerBase
+    public class OrderClassController : ControllerBase
     {
+        private static List<SimpleOrder> orders = new List<SimpleOrder>();
+
         [HttpGet]
-        public string Get()
+        public ActionResult Get() 
         {
-            return "Order API is working 🚀";
+            return Ok(orders);
+        }
+
+        [HttpPost]
+
+        public ActionResult Post(CreateOrderRequest request)
+        {
+            var order = new SimpleOrder
+            {
+                Id = orders.Count + 1,
+                Items = request.Items,
+                VAT = request.VAT
+            };
+
+            decimal subtotal = 0;
+
+            foreach(var item in order.Items)
+            {
+                 subtotal += item.Price * item.Quantity;
+
+            }
+
+            order.VatAmount = (subtotal * order.VAT) / 100m;  // ✅ correct
+            order.TotalPrice = subtotal + order.VatAmount;
+
+            orders.Add(order);
+            return Ok(order);
         }
     }
-}   
+   
+}
---
+++ RestaurantAPI/Models/SimpleOrder.cs
@@ -0,0 +1,27 @@
+﻿namespace RestaurantAPI.Models
+{
+    public class SimpleOrder
+    {
+        public int Id { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public decimal VAT { get; set; }
+
+        public decimal TotalPrice { get; set; }
+        public decimal VatAmount { get; set; }
+    }
+    public class CreateOrderRequest
+    {
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        
+        public decimal VAT { get; set; }
+    }
+
+    public class OrderItem
+    {
+        public string Name { get; set; } = "";
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
